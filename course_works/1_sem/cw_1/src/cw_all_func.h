d8f4dcbdb8adc97ed4a6e9aace5da993d7d370b9
Alinavaleyeva <112688769+Alinavaleyeva@users.noreply.github.com>
2023-07-04 11:35:49 +0500

    Delete course_works directory

--- course_works/1_sem/cw_1/src/cw_all_func.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "structs.h"
-
-void user_selection(Text_s* text);
-void print_text(Text_s* text);
--- course_works/1_sem/cw_1/src/sent_changes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "structs.h"
-
-void del_replay_sentences(Text_s* text);
-void del_special_sentence(Text_s *text);
